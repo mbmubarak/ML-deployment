a5cf40d46ca70753df597c46b9d69082f01614bb
mbmubarak <104280196+mbmubarak@users.noreply.github.com>
2022-10-13 23:38:17 -0400

    No changes

---
+++ saving_model/joblib/test.py
@@ -0,0 +1,9 @@
+import joblib
+#load the model
+model =joblib.load('diabetes_79.pkl')
+result = model.predict([[1,1,1,1,1,1,1,1]]) # random 1 is passed, it is passed 8 times becuase as 8 independent columns
+
+if result[0]==1:
+    print("diabetic")
+else:
+    print('not diabetic')
